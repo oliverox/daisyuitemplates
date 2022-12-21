93f3ce0b1f3e29ac634af02927982b65ba2f87ad
Oliver Oxenham <oliver.oxenham@gmail.com>
2022-12-21 15:15:10 +0400

    added logo

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:creator" content="@oliveroxenham" />
+        <meta name="description" content="Templates built with DaisyUI tailwind components." />
+        <meta property="og:url" content="https://daisyuitemplates.com" />
+        <meta property="og:title" content="DaisyUI Templates" />
+        <meta property="og:description" content="Templates built with DaisyUI tailwind components." />
+        <meta property="og:image" content="https://daisyuitemplates.com/logo.svg" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className="min-h-screen">
+        <Main />
+        <NextScript />
+      </body>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Html>
+  );
+}
