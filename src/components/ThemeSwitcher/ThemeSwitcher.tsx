d238332c865d2b5c5f26c89d190d863c3ced3de4
Oliver Oxenham <oliver.oxenham@gmail.com>
2022-12-22 12:57:08 +0400

    ThemeSwitcher as single toggle button

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,19 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { themeChange } from "theme-change";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 export default function ThemeSwitcher() {
+  const [theme, setTheme] = useState("dark");
   useEffect(() => {
     themeChange(false);
   }, []);
 
   return (
-    <select data-choose-theme className="select select-sm ">
-      <option value="night">
-        🌙
-      </option>
-      <option value="cmyk">
-        ☀️
-      </option>
-    </select>
+    <button
+      className="btn-ghost btn"
+      data-set-theme={theme === "dark" ? "night" : "cmyk"}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? (
+        <MoonIcon className="h-5 w-5 fill-primary stroke-primary" />
+      ) : (
+        <SunIcon className="h-5 w-5 fill-primary stroke-primary" />
+      )}
+    </button>
   );
 }
