c49665a1fa29a0345af0607e0110a38f7dea08b1
Oliver Oxenham <oliver.oxenham@gmail.com>
2022-12-22 12:34:14 +0400

    Added Theme switcher

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Link from "next/link";
 import Logo from "../Logo/Logo";
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 
 const Header = () => {
   return (
     <div className="navbar bg-base-100 max-w-7xl mx-auto max-w">
       <div className="flex-1">
-        <Logo size="sm" className="fill-neutral" />
+        <Logo size="sm" className="fill-secondary" />
+      </div>
+      <div className="mr-2">
+        <ThemeSwitcher />
       </div>
       <div className="flex-none border-l border-base-300 px-2">
         <Link className="btn btn-ghost" href="">Sign in</Link>
--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,6 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { themeChange } from "theme-change";
-import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 
 export default function ThemeSwitcher() {
   useEffect(() => {
@@ -8,23 +7,13 @@ export default function ThemeSwitcher() {
   }, []);
 
   return (
-    <div className="btn-group">
-      <button
-        aria-label="Dark theme"
-        data-set-theme="dark"
-        data-act-class="btn-accent"
-        className="btn btn-sm"
-      >
-        <MoonIcon title="Dark theme" className="h-4 w-4" />
-      </button>
-      <button
-        aria-label="Light theme"
-        data-act-class="btn-accent"
-        data-set-theme="light"
-        className="btn btn-sm"
-      >
-        <SunIcon title="Light theme" className="h-4 w-4" />
-      </button>
-    </div>
+    <select data-choose-theme className="select select-sm ">
+      <option value="night">
+        🌙
+      </option>
+      <option value="cmyk">
+        ☀️
+      </option>
+    </select>
   );
 }
