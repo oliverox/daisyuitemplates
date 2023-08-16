835c740d544d8170fa9ae4e24f8bd6f83b5cc8f4
Oliver Oxenham <oliver.oxenham@gmail.com>
2023-08-16 16:35:41 +0400

    added scrolling screenshots

--- app/page.tsx
+++ app/page.tsx
@@ -11,41 +11,128 @@ export default function Home() {
     window.cta_modal.showModal();
   };
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10 p-24">
-      <div className="text-center">
-        <span className="text-5xl leading-[1.2] font-semibold">
-          High Quality Templates
-          <br />
-          <span className="font-light italic text-4xl">for</span> DaisyUI lovers
-        </span>
-      </div>
-      <div>
-        <p className="text-gray-500 text-lg text-center max-w-md">
-          Looking for <span className="text-secondary">professional</span>,{' '}
-          <span className="text-secondary">ready-made templates</span> powered
-          by <span className="text-secondary">DaisyUI</span>? Try DaisyUI
-          Templates now.
-        </p>
-      </div>
-      <div>
-        <button
-          className="btn btn-primary btn-lg leading-[1.2]"
-          onClick={handleCtaClick}
+    <div className="min-h-screen">
+      <main className="flex flex-col items-center gap-10 p-24">
+        <div className="text-center">
+          <span className="text-5xl leading-[1.2] font-semibold">
+            High Quality Templates
+            <br />
+            <span className="font-light italic text-4xl">for</span> DaisyUI
+            lovers
+          </span>
+        </div>
+        <div>
+          <p className="text-gray-500 text-lg text-center max-w-md">
+            Looking for <span className="text-secondary">professional</span>,{' '}
+            <span className="text-secondary">ready-made templates</span> powered
+            by <span className="text-secondary">DaisyUI</span>? Try DaisyUI
+            Templates now.
+          </p>
+        </div>
+        <div>
+          <button
+            className="btn btn-primary btn-lg leading-[1.2]"
+            onClick={handleCtaClick}
+          >
+            Try DaisyUI Templates ➔
+          </button>
+        </div>
+        <dialog id="cta_modal" className="modal">
+          <form method="dialog" className="modal-box">
+            <h3 className="font-bold text-lg">
+              DaisyUI Templates is coming soon...
+            </h3>
+            <p className="py-4">Thanks for your patience.</p>
+            <div className="modal-action">
+              <button className="btn">Close</button>
+            </div>
+          </form>
+        </dialog>
+      </main>
+      <div
+        className="w-full inline-flex flex-nowrap overflow-hidden mb-8
+                    [mask-image:_linear-gradient(to_right,transparent_0,_black_38px,_black_calc(100%-38px),transparent_100%)]"
+      >
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-3 
+                      [&_img]:max-w-none animate-infinite-scroll"
         >
-          Try DaisyUI Templates ➔
-        </button>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-3 
+                    [&_img]:max-w-none animate-infinite-scroll"
+          aria-hidden="true"
+        >
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+          <li>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </li>
+        </ul>
       </div>
-      <dialog id="cta_modal" className="modal">
-        <form method="dialog" className="modal-box">
-          <h3 className="font-bold text-lg">
-            DaisyUI Templates is coming soon...
-          </h3>
-          <p className="py-4">Thanks for your patience.</p>
-          <div className="modal-action">
-            <button className="btn">Close</button>
+      {/* <div className="w-full overflow-hidden h-[350px] overflow-x-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+        <div className="animate-infinite-scroll flex flex-row gap-3">
+          <div className="flex flex-row flex-nowrap gap-3">
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
           </div>
-        </form>
-      </dialog>
-    </main>
+          <div className="flex flex-row flex-nowrap gap-3">
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+            <div className="w-[300px] h-[300px] border-0 drop-shadow-lg rounded-md bg-white border-spacing-1 border-gray-300"></div>
+          </div>
+        </div>
+      </div> */}
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,15 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-figtree)'],
       },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 50s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },                          
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
