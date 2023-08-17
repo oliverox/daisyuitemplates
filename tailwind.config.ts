9752afd8f71b6b101234a92c665b1a73180c60c5
Oliver Oxenham <oliver.oxenham@gmail.com>
2023-08-17 12:56:31 +0400

    Added components folder, toggable themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,14 +19,26 @@ const config: Config = {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
         }
-      },                          
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          "--gradient1": "#ffffff",
+          "--gradient2": "#00C9FF"
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          "--gradient1": "#1e3a8a",
+          "--gradient2": "#2b3440"
+        }
+      }
+    ],
+    darkTheme: "dark"
+  }
 };
 export default config;
