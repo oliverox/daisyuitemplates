fea2610bebadfe208b2a58c94227106858b59103
Oliver Oxenham <oliver.oxenham@gmail.com>
2023-08-17 11:04:17 +0400

    dark mode icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,40 @@ export const metadata: Metadata = {
   description: 'High quality templates for DaisyUI lovers.',
 };
 
+function NavbarMenu() {
+  return (
+    <>
+      <li>
+        <Link href="/products">Products</Link>
+      </li>
+      <li>
+        <a>Freebies</a>
+      </li>
+      <li>
+        <a>Account</a>
+      </li>
+      <li>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5 dark:stroke-accent stroke-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+        </a>
+      </li>
+    </>
+  );
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -33,24 +67,47 @@ export default function RootLayout({
                 </g>
               </svg>
             </div>
-            <Link href="/" className="text-xl hidden sm:block font-mono dark:text-white font-bold">
+            <Link
+              href="/"
+              className="text-xl hidden sm:block font-mono dark:text-white font-bold"
+            >
               DaisyUI Templates
             </Link>
-            <Link href="/" className="text-2xl sm:hidden font-mono dark:text-white font-bold">
+            <Link
+              href="/"
+              className="text-2xl sm:hidden font-mono dark:text-white font-bold"
+            >
               DT
             </Link>
           </div>
           <div className="flex-none">
-            <ul className="menu menu-horizontal px-1 font-semibold dark:text-white">
+            <ul className="menu menu-horizontal px-1 font-semibold md:hidden z-10">
               <li>
-                <Link href="/products">Products</Link>
+                <details>
+                  <summary>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+                      />
+                    </svg>
+                  </summary>
+                  <ul className="p-2 bg-base-100 !-ml-8">
+                    <NavbarMenu />
+                  </ul>
+                </details>
               </li>
-              <li>
-                <a>Freebies</a>
-              </li>
-              <li>
-                <a>Account</a>
-              </li>
+            </ul>
+            <ul className="menu menu-horizontal px-1 font-semibold dark:text-white hidden md:inline-flex">
+              <NavbarMenu />
             </ul>
           </div>
         </div>
