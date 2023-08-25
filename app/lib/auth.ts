5eb0df1b75e1c1c00d7373a373fdf71c61738853
Oliver Oxenham <oliver.oxenham@gmail.com>
2023-08-25 13:32:32 +0400

    Enabled sign in with Google

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 export const authOptions: NextAuthOptions = {
   // Secret for Next-auth, without this JWT encryption/decryption won't work
   secret: process.env.NEXTAUTH_SECRET,
@@ -10,5 +11,9 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_APP_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_APP_CLIENT_SECRET as string,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
   ],
 };
